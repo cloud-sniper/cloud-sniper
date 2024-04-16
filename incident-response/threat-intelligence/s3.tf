76e1531952be87de8b2c40fa2f4fe54fb9a88d81
nicolasriverocorvalan <riveronicolas@gmail.com>
2024-04-16 16:53:19 -0300

    ir-ti

--- incident-response/threat-intelligence/s3.tf
+++ incident-response/threat-intelligence/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "cloud_sniper_s3_bucket_data_store" {
-  for_each = local.hub_account_id == data.aws_caller_identity.current.account_id ? { hub : true } : {}
+  for_each = var.cloud_sniper_hub_account_id == data.aws_caller_identity.current.account_id ? { hub : true } : {}
   bucket   = join("-", [var.cloud_sniper_data_store, data.aws_region.current.name])
 }
 
@@ -12,7 +12,7 @@ resource "aws_s3_bucket_public_access_block" "cloud_sniper_s3_bucket_access_bloc
 }
 
 resource "aws_s3_object" "cloud_sniper_beaconing_file_upload" {
-  for_each = local.hub_account_id == data.aws_caller_identity.current.account_id ? { hub : true } : {}
+  for_each = var.cloud_sniper_hub_account_id == data.aws_caller_identity.current.account_id ? { hub : true } : {}
   bucket   = aws_s3_bucket.cloud_sniper_s3_bucket_data_store["hub"].id
   key      = "cloud_sniper_beaconing_detection.zip"
   source   = "../analytics/target/cloud_sniper_beaconing_detection.zip"
